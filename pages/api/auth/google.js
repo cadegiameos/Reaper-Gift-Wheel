1c612f068d62f0296007f706ee3ade3f1b20ff2f
Cade Giameos <cadegiameos@gmail.com>
2025-10-22 16:14:00 +1100

    error fix

--- pages/api/auth/google.js
+++ pages/api/auth/google.js
@@ -1,16 +1,12 @@
-// pages/api/auth/google.js
-export default function handler(req, res) {
-  const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
-
 export default function handler(req, res) {
   const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
 
   const options = {
     redirect_uri: `${process.env.NEXT_PUBLIC_SITE_URL}/api/auth/callback`,
     client_id: process.env.GOOGLE_CLIENT_ID,
-    access_type: "offline", // ensures we get a refresh token
+    access_type: "offline", // always returns refresh token
     response_type: "code",
-    prompt: "consent", // always ask for consent to ensure refresh token
+    prompt: "consent", // ask every time to ensure refresh token
     scope: "https://www.googleapis.com/auth/youtube.readonly",
   };
 
